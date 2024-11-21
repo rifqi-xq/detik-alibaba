5718437ff5a2da9cd883e55e6ba7f234cf7ca2ea
rifqi sentosa <rifqi@xquisite.ai>
2024-11-21 17:58:37 +0700

    Commenting pubsub service

--- analytic-collector/services/gcppubsub/pubsub.go
+++ analytic-collector/services/gcppubsub/pubsub.go
@@ -1,77 +1,77 @@
-package gcppubsub
+// package gcppubsub
 
-import (
-	"errors"
-	"os"
+// import (
+// 	"errors"
+// 	"os"
 
-	"cloud.google.com/go/pubsub"
-	"github.com/inconshreveable/log15"
-	"golang.org/x/net/context"
-)
+// 	"cloud.google.com/go/pubsub"
+// 	"github.com/inconshreveable/log15"
+// 	"golang.org/x/net/context"
+// )
 
-// GcpPubSub holds GCP PubSub struct
-// example: https://github.com/GoogleCloudPlatform/golang-samples/blob/master/pubsub/topics/main.go
-type GcpPubSub struct {
-	ctx       context.Context
-	client    *pubsub.Client
-	topicGET  *pubsub.Topic
-	topicPOST *pubsub.Topic
-}
+// // GcpPubSub holds GCP PubSub struct
+// // example: https://github.com/GoogleCloudPlatform/golang-samples/blob/master/pubsub/topics/main.go
+// type GcpPubSub struct {
+// 	ctx       context.Context
+// 	client    *pubsub.Client
+// 	topicGET  *pubsub.Topic
+// 	topicPOST *pubsub.Topic
+// }
 
-// NewGcpPubSub initiate new GCP PubSub
-func NewGcpPubSub(ctx context.Context, projectID, topicGET, topicPOST string) (*GcpPubSub, error) {
-	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
-		log15.Error("GOOGLE_APPLICATION_CREDENTIALS environment variable not found")
-		return nil, errors.New("GOOGLE_APPLICATION_CREDENTIALS environment variable not found")
-	}
+// // NewGcpPubSub initiate new GCP PubSub
+// func NewGcpPubSub(ctx context.Context, projectID, topicGET, topicPOST string) (*GcpPubSub, error) {
+// 	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
+// 		log15.Error("GOOGLE_APPLICATION_CREDENTIALS environment variable not found")
+// 		return nil, errors.New("GOOGLE_APPLICATION_CREDENTIALS environment variable not found")
+// 	}
 
-	client, err := pubsub.NewClient(ctx, projectID)
-	if err != nil {
-		log15.Error("Failed to connect to GCP " + err.Error())
-		return nil, err
-	}
+// 	client, err := pubsub.NewClient(ctx, projectID)
+// 	if err != nil {
+// 		log15.Error("Failed to connect to GCP " + err.Error())
+// 		return nil, err
+// 	}
 
-	gcp := &GcpPubSub{
-		ctx:       ctx,
-		client:    client,
-		topicGET:  client.Topic(topicGET),
-		topicPOST: client.Topic(topicPOST),
-	}
+// 	gcp := &GcpPubSub{
+// 		ctx:       ctx,
+// 		client:    client,
+// 		topicGET:  client.Topic(topicGET),
+// 		topicPOST: client.Topic(topicPOST),
+// 	}
 
-	return gcp, nil
-}
+// 	return gcp, nil
+// }
 
-// Close connection
-func (g *GcpPubSub) Close() error {
-	return g.client.Close()
-}
+// // Close connection
+// func (g *GcpPubSub) Close() error {
+// 	return g.client.Close()
+// }
 
-// PublishGET is function to publish message from GET to PubSub
-func (g *GcpPubSub) PublishGET(messageJSON []byte) (*string, error) {
-	result := g.topicGET.Publish(g.ctx, &pubsub.Message{
-		Data: messageJSON,
-	})
+// // PublishGET is function to publish message from GET to PubSub
+// func (g *GcpPubSub) PublishGET(messageJSON []byte) (*string, error) {
+// 	result := g.topicGET.Publish(g.ctx, &pubsub.Message{
+// 		Data: messageJSON,
+// 	})
 
-	serverID, err := result.Get(g.ctx)
-	if err != nil {
-		log15.Error(err.Error())
-		return nil, err
-	}
+// 	serverID, err := result.Get(g.ctx)
+// 	if err != nil {
+// 		log15.Error(err.Error())
+// 		return nil, err
+// 	}
 
-	return &serverID, nil
-}
+// 	return &serverID, nil
+// }
 
-// PublishPOST is function to publish message from POST to PubSub
-func (g *GcpPubSub) PublishPOST(messageJSON []byte) (*string, error) {
-	result := g.topicPOST.Publish(g.ctx, &pubsub.Message{
-		Data: messageJSON,
-	})
+// // PublishPOST is function to publish message from POST to PubSub
+// func (g *GcpPubSub) PublishPOST(messageJSON []byte) (*string, error) {
+// 	result := g.topicPOST.Publish(g.ctx, &pubsub.Message{
+// 		Data: messageJSON,
+// 	})
 
-	serverID, err := result.Get(g.ctx)
-	if err != nil {
-		log15.Error(err.Error())
-		return nil, err
-	}
+// 	serverID, err := result.Get(g.ctx)
+// 	if err != nil {
+// 		log15.Error(err.Error())
+// 		return nil, err
+// 	}
 
-	return &serverID, nil
-}
+// 	return &serverID, nil
+// }
